7e1e596ef87d40c25e93f396330cde32c65d2daf
jf15558 <50243608+jf15558@users.noreply.github.com>
2026-05-21 14:39:58 +0100

    bugszzzz

--- R/link_islands.R
+++ R/link_islands.R
@@ -264,7 +264,7 @@ link_islands <- function(geog, klink = NULL, verbose = T) {
     message("No islands found in any layers, no links will be returned")
     return(NULL)
   } else {
-    geog$lnks <- unique(rbind(geog$links, do.call(rbind, res_list)))
+    geog$links <- unique(rbind(geog$links, do.call(rbind, res_list)))
     #geog$links <- st_wrap_dateline(lnks, options = c("WRAPDATELINE=YES", "DATELINEOFFSET=180"))
     return(geog)
   }
