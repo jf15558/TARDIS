652b9042451ad0b63cb0f86b19fcf5a7f2dadbbc
jf15558 <50243608+jf15558@users.noreply.github.com>
2026-05-22 08:47:19 +0100

    Update link_islands.R

--- R/link_islands.R
+++ R/link_islands.R
@@ -80,9 +80,9 @@
 
 link_islands <- function(geog, klink = NULL, verbose = T) {
   #
-  geog = rasts
-  klink = 1
-  verbose = T
+  # geog = rasts
+  # klink = 1
+  # verbose = T
   #
   if(!exists("geog")) {
     stop("Supply geog as a geoglist from rast_to_geoglist()")
