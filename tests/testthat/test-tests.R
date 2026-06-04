0ee9b019e57659b9689c8771bdb5ac9f49506bee
jf15558 <50243608+jf15558@users.noreply.github.com>
2026-06-04 22:08:52 +0100

    eliminate tests for now

--- tests/testthat/test-tests.R
+++ tests/testthat/test-tests.R
@@ -1,29 +1,3 @@
-## SETUP ##
-
-# load required libraries
-library(terra)
-library(TARDIS)
-library(testthat)
-
-# set up objects
-gal <- galapagos()
-gal_m <- classify(gal, matrix(c(-Inf, 0, NA, 0, Inf, 1), ncol = 3, byrow = T), right = F)
-hexes <- rast_to_geoglist(gal, gal_m, as.hex = T, hex = 6)
-hexes <- link_islands(hexes, klink = 1)
-
-
-## FUNCTIONS ##
-
-# build_tardis
-htd <- build_tardis(hexes, times = c(seq(2.25, 0, -0.5), 0))
-
-# cost_surface
-
-# cretaceous
-
-# cumulative_cost
-
-# extract_geoglist
 
 # galapagos
 
